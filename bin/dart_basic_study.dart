30ad8802a16d439b34eb722b6bd94896799d8e63
YuJin <cyj920309@naver.com>
2023-01-26 22:45:15 +0900

    variable

--- bin/dart_basic_study.dart
+++ bin/dart_basic_study.dart
@@ -1,1 +1,10 @@
-void main() {}
+void main() {
+  // 변수
+  var variableName = 'value';
+  variableName = 'bora';
+  print(variableName);
+
+  String? variablesName = 'value';
+  variablesName = null;
+  print(variablesName);
+}
